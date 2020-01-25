78a5f8d939ae5ec2661aa81bfc516941c820d34e
eriknson <eriks@mail.se>
2020-01-25 14:06:00 +0100

    -

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,9 +6,10 @@ class Footer extends Component {
       <div className='footer'>
         <h2>Footer placeholder</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
         </p>
       </div>
     );
